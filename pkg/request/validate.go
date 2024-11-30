18cc9f5caa3fffc7828dff2bfa455dbfd0557100
Andrey S <falkonirk@gmail.com>
2024-11-30 10:08:54 +0300

    Revert "init commit"
    This reverts commit 6e81c183b9637bda61f76fdf1f0a4e601daa30b0.

--- pkg/request/validate.go
+++
@@ -1,9 +0,0 @@
-package request
-
-import "github.com/go-playground/validator/v10"
-
-func CheckIfValid[T any](data T) error {
-	validate := validator.New()
-	err := validate.Struct(data)
-	return err
-}
